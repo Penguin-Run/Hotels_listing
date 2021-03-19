35fb9e5b3154229d2dfd51df7bfe5372886e88dc
Penguin-Run <penguinlgb@gmail.com>
2021-03-19 23:51:10 +0300

    добавил возможность поменять язык самих записей с Mobility

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  around_action :switch_global_locale, :switch_mobility_locale
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
+  # TODO: баг - иногда глобальная локаль все равно меняется при изменении Mobility локали
+  def switch_global_locale(&action)
+    locale = params[:locale]
+    I18n.with_locale(locale, &action) unless locale.nil?
+    yield
+  end
+
+  def switch_mobility_locale(&action)
+    locale = params[:mobility]
+    unless locale.nil?
+      symbol_locale = :en
+      symbol_locale = :ru if locale == 'ru'
+
+      Mobility.with_locale(symbol_locale, &action)
+    end
+    yield
   end
 end
